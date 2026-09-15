8b804b27ce6bfd9adb62828eaf201fa025f7119d
AmethyxGG <dani.craig86@proton.me>
2026-09-15 13:45:53 +0100

    Remove realms, update character type

--- enum/Realm.ts
+++
@@ -1,8 +0,0 @@
-export enum Realm {
-  Draenor = "Draenor",
-  Kazzak = "Kazzak",
-  Lightbringer = "Lightbringer",
-  Magtheridon = "Magtheridon",
-  Silvermoon = "Silvermoon",
-  TarrenMill = "TarrenMill",
-}
--- types/Character.d.ts
+++ types/Character.d.ts
@@ -1,6 +1,5 @@
 import type { WoWClass } from "@amethyxgg/wow-forever-data/WoWClass"
 import type { CraftedItem } from "@amethyxgg/wow-forever-data/CraftedItem"
-import { Realm } from "@amethyxgg/wow-forever-data/Realm"
 import { Role } from "@amethyxgg/wow-forever-data/Role";
 import { Race } from "@amethyxgg/wow-forever-data/Race";
 
@@ -31,9 +30,16 @@ export type CharacterProfessions = Partial<Record<WowProfession, ProfessionProgr
 
 export type Character = {
     name: string;
-    realm: Realm;
+    ruleset: "normal" | "hardcore" | "pvp" | "roleplay";
+    faction?: "alliance" | "horde";
     race?: Race;
     wowclass: WoWClass;
     professions?: CharacterProfessions;
     role?: Role[];
+    level?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+        | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
+        | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30
+        | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40
+        | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50
+        | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60;
 };
